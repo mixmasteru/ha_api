c7efafe3565573b8ff4234873c1a13c136c974ea
Ulrich Pech <ulrichpech@gmail.com>
2015-10-23 17:35:05 +0200

    test dispatcher

--- web/test_dispatch.php
+++ web/test_dispatch.php
@@ -1,7 +1,21 @@
 <?php
 /**
- * Created by IntelliJ IDEA.
- * User: upech
- * Date: 22.10.15
- * Time: 17:37
+ *
+ * @see http://stackoverflow.com/questions/19821082/collate-several-xdebug-coverage-results-into-one-report
  */
+
+// autoloader
+require_once './vendor/autoload.php';
+
+//only works for one call
+$coverage = new PHP_CodeCoverage();
+$coverage->filter()->addDirectoryToWhitelist('./src/');
+$coverage->start('Site coverage');
+
+require_once 'dispatch.php';
+
+$coverage->stop();
+$writer = new PHP_CodeCoverage_Report_HTML(35, 70);
+$writer->process($coverage, 'coverage');
+$writer = new PHP_CodeCoverage_Report_Clover();
+$writer->process($coverage, 'coverage.xml');
