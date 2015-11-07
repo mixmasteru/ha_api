122eb80a6cbae05983eda9f6d838ea961daca06e
mixmasteru <ulrichpech@gmail.com>
2015-11-07 18:34:46 +0100

    more humi tests

--- tests/HumidityTest.php
+++ tests/HumidityTest.php
@@ -12,6 +12,16 @@ class HumidityTests extends BaseTest
         parent::setUp();
     }
 
+    public function testGET404()
+    {
+        $datetime   = "20151201-231100";
+        $request = $this->client->get('/humi/1/'.$datetime.'/');
+        $response = $request->send();
+
+        $this->assertEquals(404, $response->getStatusCode(),"wrong response code, not 404: ".$request->getResponse());
+        $this->assertEquals('The server has not found anything matching the Request-URI', $response->getBody());
+    }
+
     public function testPUTGET()
     {
         $datetime   = "20151204-231100";
@@ -20,11 +30,11 @@ class HumidityTests extends BaseTest
 
         $request = $this->client->put('/humi/1/'.$datetime.'/'.$temp.'/');
         $response = $request->send();
-        $this->assertEquals(200, $response->getStatusCode(),"wrong response code: ".$request->getResponse());
+        $this->assertEquals(200, $response->getStatusCode(),"wrong response code, not 200: ".$request->getResponse());
 
         $request = $this->client->get('/humi/1/'.$datetime.'/');
         $response = $request->send();
-        $this->assertEquals(200, $response->getStatusCode(),"wrong response code: ".$request->getResponse());
+        $this->assertEquals(200, $response->getStatusCode(),"wrong response code, not 200: ".$request->getResponse());
         $this->assertEquals($json, $response->getBody(true));
     }
 }
